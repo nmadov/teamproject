b670993ea72eb08494498ab62190018428617658
Liam <LiamMerrill@gmail.com>
2015-05-07 08:33:39 -0400

    Data on the page

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ $(document).ready(function(){
 
 
 $.ajax({
-	    url: 'http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json&api-key=bea85d618afa5b5103225dd54fecd937:0:69746075',
-	    success: function(data) {
-	    var arr = createUniArray(data)
-	      putOnPage(abcArr);
-	      runSearch();
-	    }
-	});
+	    url: 'http://api.nytimes.com/svc/books/v3/lists/names?api-key=b1afdfe3bd9d8e3e8c1f3f489838a282:18:65509848',
+	    success: doThingWithData
+});
+
+function doThingWithData(data) {
+	
+	var results = data.results;
+	for(var i=0; i<results.length; i++){
+		var list = results[i];
+		var name = list.list_name;
+		$('<h1>').text(name).appendTo(document.body);
+	}
+}
+
 function putOnPage(data){
 
 		//define underscore template
