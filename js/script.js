2976cdc06a972b0dd4a2876f6b06417c9ab55c66
Liam <LiamMerrill@gmail.com>
2015-05-11 20:06:41 -0400

    Data on Page, Title, Author

--- js/script.js
+++ js/script.js
@@ -3,32 +3,28 @@ $(document).ready(function(){
 
 
 $.ajax({
-	    url: 'http://api.nytimes.com/svc/books/v3/lists/names?api-key=b1afdfe3bd9d8e3e8c1f3f489838a282:18:65509848',
+	    url: 'http://api.nytimes.com/svc/books/v2/lists/hardcover-fiction.jsonp?callback=books&api-key=b1afdfe3bd9d8e3e8c1f3f489838a282:18:65509848',
+	    dataType: "jsonp",
+	    jsonpCallback: "books",
 	    success: doThingWithData
 });
 
 function doThingWithData(data) {
 	
+	console.log(books);
 	var results = data.results;
-	for(var i=0; i<results.length; i++){
-		var list = results[i];
-		var name = list.list_name;
-		$('<h1>').text(name).appendTo(document.body);
-	}
+	 for(var i=0; i<results.length; i++){
+	 	var list = results[i];
+	 	var title = list.book_details[0].title;
+	 	var author = list.book_details[0].author;
+	 	$('<h1>').text(title).appendTo(document.body);
+	 	$('<h2>').text("by " + author).appendTo(document.body);
+	 }
 }
 
-function putOnPage(data){
 
-		//define underscore template
-		var listItemTemplate = _.template("<li><a target='_blank' href='<%= url %>'><h3><%= name %> &raquo;</h3></a></li>");
 
-		_.each(arr,function(uni,index){
-			//pass object to list item template
-			var listItem = listItemTemplate(uni);
-
-			//append to page
-			$('#books').append(listItem);
-		});
-
-	}
+	
 });
+
+// http://api.nytimes.com/svc/books/{version}/lists/best-sellers/history[.response_format]?{search-param1=value1}&[...]&[optional-param1=value1]&[...]&api-key={b1afdfe3bd9d8e3e8c1f3f489838a282:18:65509848}
